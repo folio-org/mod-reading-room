385a37e1b3518827989a2ebcb74572eadb1146c6
Adesh_Singh <Adesh_Singh@epam.com>
2024-04-23 16:06:23 +0530

    [MODRR-8]-Added GET endpoint for patron permission.

--- src/test/java/org/folio/readingroom/controller/PatronPermissionControllerTest.java
+++ src/test/java/org/folio/readingroom/controller/PatronPermissionControllerTest.java
@@ -3,12 +3,16 @@ package org.folio.readingroom.controller;
 import static org.folio.readingroom.domain.dto.AccessLog.ActionEnum.ALLOWED;
 import static org.folio.readingroom.domain.dto.PatronPermission.AccessEnum.NOT_ALLOWED;
 import static org.folio.readingroom.utils.HelperUtils.READING_ROOM_ID_FOR_PATRON_TEST;
+import static org.folio.readingroom.utils.HelperUtils.READING_ROOM_ID_FOR_PATRON_TEST_1;
+import static org.folio.readingroom.utils.HelperUtils.SERVICE_POINT_ID1;
 import static org.folio.readingroom.utils.HelperUtils.SERVICE_POINT_ID4;
 import static org.folio.readingroom.utils.HelperUtils.SERVICE_POINT_NAME3;
+import static org.folio.readingroom.utils.HelperUtils.SERVICE_POINT_NAME4;
 import static org.folio.readingroom.utils.HelperUtils.createPatronPermission;
 import static org.folio.readingroom.utils.HelperUtils.createReadingRoom;
 import static org.folio.readingroom.utils.HelperUtils.createServicePoint;
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -132,6 +136,82 @@ class PatronPermissionControllerTest extends BaseIT {
           .contentType(MediaType.APPLICATION_JSON)
           .accept(MediaType.APPLICATION_JSON))
       .andExpect(status().is2xxSuccessful());
+
+    this.mockMvc.perform(
+        get("/reading-room-patron-permission/2205005b-ca51-4a04-87fd-938eefa8f6df")
+          .headers(defaultHeaders())
+          .contentType(MediaType.APPLICATION_JSON)
+          .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().is2xxSuccessful())
+      .andExpect(jsonPath("$[0].readingRoomId").value("7a5abc9f-f3d7-4856-b8d7-6712462ca009"))
+      .andExpect(jsonPath("$.[0].userId").value("2205005b-ca51-4a04-87fd-938eefa8f6df"))
+      .andExpect(jsonPath("$.[0].access").value(ALLOWED.getValue()))
+      .andExpect(jsonPath("$", hasSize(1)));
+  }
+
+  @Test
+  void testGetPatronPermissionForDeleted() throws Exception {
+    removeReadingRoomIfExists();
+    ReadingRoom readingRoom = createReadingRoom(READING_ROOM_ID_FOR_PATRON_TEST, false);
+    readingRoom.setName("TestPatronRR");
+    var servicePoints = Set.of(createServicePoint(SERVICE_POINT_ID4, SERVICE_POINT_NAME3));
+    readingRoom.servicePoints(servicePoints);
+
+    this.mockMvc.perform(
+        post("/reading-room")
+          .content(asJsonString(readingRoom))
+          .headers(defaultHeaders())
+          .contentType(MediaType.APPLICATION_JSON)
+          .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isCreated());
+
+    ReadingRoom readingRoom2 = createReadingRoom(READING_ROOM_ID_FOR_PATRON_TEST_1, false);
+    readingRoom2.setName("TestPatronRR1");
+    var servicePoints2 = Set.of(createServicePoint(SERVICE_POINT_ID1, SERVICE_POINT_NAME4));
+    readingRoom2.servicePoints(servicePoints2);
+
+    this.mockMvc.perform(
+        post("/reading-room")
+          .content(asJsonString(readingRoom2))
+          .headers(defaultHeaders())
+          .contentType(MediaType.APPLICATION_JSON)
+          .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isCreated());
+
+    this.mockMvc.perform(
+        get("/reading-room-patron-permission/2205005b-ca51-4a04-87fd-938eefa8f6df")
+          .headers(defaultHeaders())
+          .contentType(MediaType.APPLICATION_JSON)
+          .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().is2xxSuccessful())
+      .andExpect(jsonPath("$[0].readingRoomId").value("8a5abc9f-f3d7-4856-b8d7-6712462ca009"))
+      .andExpect(jsonPath("$.[0].userId").value("2205005b-ca51-4a04-87fd-938eefa8f6df"))
+      .andExpect(jsonPath("$.[0].access").value(NOT_ALLOWED.getValue()))
+      .andExpect(jsonPath("$", hasSize(2)));
+
+    PatronPermission patronPermission = createPatronPermission(UUID.randomUUID(),
+      READING_ROOM_ID_FOR_PATRON_TEST, UUID.fromString("2205005b-ca51-4a04-87fd-938eefa8f6df"));
+    this.mockMvc.perform(
+        put("/reading-room-patron-permission/2205005b-ca51-4a04-87fd-938eefa8f6df")
+          .content(asJsonString(patronPermission))
+          .headers(defaultHeaders())
+          .contentType(MediaType.APPLICATION_JSON)
+          .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().is2xxSuccessful());
+
+    PatronPermission patronPermission2 = createPatronPermission(UUID.randomUUID(),
+      READING_ROOM_ID_FOR_PATRON_TEST_1, UUID.fromString("2205005b-ca51-4a04-87fd-938eefa8f6df"));
+    this.mockMvc.perform(
+        put("/reading-room-patron-permission/2205005b-ca51-4a04-87fd-938eefa8f6df")
+          .content(asJsonString(patronPermission2))
+          .headers(defaultHeaders())
+          .contentType(MediaType.APPLICATION_JSON)
+          .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().is2xxSuccessful());
+
+    this.mockMvc.perform(delete("/reading-room/" + READING_ROOM_ID_FOR_PATRON_TEST_1)
+        .headers(defaultHeaders()))
+      .andExpect(status().isNoContent());
 
     this.mockMvc.perform(
         get("/reading-room-patron-permission/2205005b-ca51-4a04-87fd-938eefa8f6df")
--- src/test/java/org/folio/readingroom/utils/HelperUtils.java
+++ src/test/java/org/folio/readingroom/utils/HelperUtils.java
@@ -13,6 +13,7 @@ public class HelperUtils {
 
   public static final UUID READING_ROOM_ID = UUID.fromString("7a5abc9f-f3d7-4856-b8d7-6712462ca008");
   public static final UUID READING_ROOM_ID_FOR_PATRON_TEST = UUID.fromString("7a5abc9f-f3d7-4856-b8d7-6712462ca009");
+  public static final UUID READING_ROOM_ID_FOR_PATRON_TEST_1 = UUID.fromString("8a5abc9f-f3d7-4856-b8d7-6712462ca009");
   public static final UUID SERVICE_POINT_ID1 = UUID.fromString("7c5abc9f-f3d7-4856-b8d7-6712462ca008");
   public static final UUID SERVICE_POINT_ID2 = UUID.fromString("9765c56f-608d-4c93-94fe-f365f16e6970");
   public static final UUID SERVICE_POINT_ID3 = UUID.fromString("9765c56f-608d-4c93-94fe-f365f16e6971");
@@ -23,6 +24,7 @@ public class HelperUtils {
   public static final String SERVICE_POINT_NAME1 = "servicePoint1";
   public static final String SERVICE_POINT_NAME2 = "servicePoint2";
   public static final String SERVICE_POINT_NAME3 = "servicePoint3";
+  public static final String SERVICE_POINT_NAME4 = "servicePoint4";
 
   public static ReadingRoom createReadingRoom(UUID readingRoomId, boolean ispublic) {
     ReadingRoom readingRoom = new ReadingRoom();

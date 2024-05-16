82ff41912b2bfebab4d404184f8627bf2265a36f
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2024-05-16 16:09:04 +0530

    MODRR-18 Setting up the created details in updated details if updated details are null

--- src/main/java/org/folio/readingroom/service/converter/Mapper.java
+++ src/main/java/org/folio/readingroom/service/converter/Mapper.java
@@ -2,6 +2,7 @@ package org.folio.readingroom.service.converter;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import org.folio.readingroom.domain.dto.AccessLog;
 import org.folio.readingroom.domain.dto.PatronPermission;
 import org.folio.readingroom.domain.dto.ReadingRoom;
@@ -63,8 +64,8 @@ public interface Mapper {
 
   @Mapping(target = "metadata.createdByUserId", source = "createdBy")
   @Mapping(target = "metadata.createdDate", source = "createdDate")
-  @Mapping(target = "metadata.updatedByUserId", source = "updatedBy")
-  @Mapping(target = "metadata.updatedDate", source = "updatedDate")
+  @Mapping(target = "metadata.updatedByUserId", expression = "java(setUpdatedByUserId(patronPermissionProjection))")
+  @Mapping(target = "metadata.updatedDate", expression = "java(setUpdatedDate(patronPermissionProjection))")
   PatronPermission toDto(PatronPermissionProjection patronPermissionProjection);
 
   default ReadingRoomCollection toDto(Page<ReadingRoomEntity> readingRoomEntityPage, boolean includeDeleted) {
@@ -76,6 +77,16 @@ public interface Mapper {
         .toList();
     }
     return new ReadingRoomCollection(toDtoList(readingRooms), readingRooms.size());
+  }
+
+  default String setUpdatedDate(PatronPermissionProjection patronPermissionProjection) {
+    return patronPermissionProjection.getUpdatedDate() != null
+      ? patronPermissionProjection.getUpdatedDate() : patronPermissionProjection.getCreatedDate();
+  }
+
+  default UUID setUpdatedByUserId(PatronPermissionProjection patronPermissionProjection) {
+    return patronPermissionProjection.getUpdatedBy() != null
+      ? patronPermissionProjection.getUpdatedBy() : patronPermissionProjection.getCreatedBy();
   }
 
   @AfterMapping
